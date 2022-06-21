9e359ed76c74ccfffc580d6ae611efbbfc0ef33d
pien <pincenna@gmail.com>
2022-06-22 02:29:11 +0700

    Finishing

--- app/src/main/java/xyz/alviencenna/uas/MainActivity.java
+++ app/src/main/java/xyz/alviencenna/uas/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,60 +23,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bottomnavbar);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.container, new fragmentHome()).commit();
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                CharSequence title = item.getTitle();
+        BottomNavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setSelectedItemId(R.id.home);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        CharSequence title = item.getTitle();
 
-                switch (id) {
-                    case R.id.home:
-                        fragmentManager.beginTransaction().replace(R.id.container, new fragmentHome()).commit();
-                        mDrawerLayout.close();
-                        Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.about:
-                        fragmentManager.beginTransaction().replace(R.id.container, new fragmentAbout()).commit();
-                        mDrawerLayout.close();
-                        Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.portfolio:
-                        fragmentManager.beginTransaction().replace(R.id.container, new fragmentPortfolio()).commit();
-                        mDrawerLayout.close();
-                        Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.blog:
-                        fragmentManager.beginTransaction().replace(R.id.container, new fragmentBlog()).commit();
-                        mDrawerLayout.close();
-                        Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.contact:
-                        fragmentManager.beginTransaction().replace(R.id.container, new fragmentContact()).commit();
-                        mDrawerLayout.close();
-                        Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-                return true;
-            }
-        });
+                        switch (id) {
+                            case R.id.home:
+                                fragmentManager.beginTransaction().replace(R.id.container, new fragmentHome()).commit();
+                                Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.about:
+                                fragmentManager.beginTransaction().replace(R.id.container, new fragmentAbout()).commit();
+                                Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.portfolio:
+                                fragmentManager.beginTransaction().replace(R.id.container, new fragmentPortfolio()).commit();
+                                Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.blog:
+                                fragmentManager.beginTransaction().replace(R.id.container, new fragmentBlog()).commit();
+                                Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.contact:
+                                fragmentManager.beginTransaction().replace(R.id.container, new fragmentContact()).commit();
+                                Toast.makeText(MainActivity.this, title + " selected", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
 
-        mDrawerLayout = findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.buka, R.string.tutup);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
